97f99b1889ec3a5d2f87172124d367d7c01b30b9
Jesus Rivera <dani882@gmail.com>
2019-06-01 21:03:03 -0400

    adding header to nginx.conf and default.conf

--- pushecr.sh
+++ pushecr.sh
@@ -6,3 +6,29 @@ eval $(aws ecr get-login --region us-east-2 --no-include-email)
 
 
 
+##############################################################
+##############################################################
+
+#!/bin/bash
+if (( "$#" != 1 ))
+then
+    echo "Usage:
+./ecr_push [environment]"
+exit 1
+fi
+if [[ $@ != acceptance && $@ != staging && $@ != production ]] ;
+then
+  echo "Environment must be either Acceptance, Staging or Production."
+  exit 1
+elif [[ $@ = staging || acceptance ]] ;
+then
+ AWS_ACCOUNT=XXXXXXXXXXXX
+elif [[ $@ = production ]] ;
+then
+  AWS_ACCOUNT=XXXXXXXXXXXX
+fi
+REPO="$AWS_ACCOUNT.dkr.ecr.us-west-2.amazonaws.com/rails_application-$@"
+IMAGE=$REPO:latest-$@
+$(aws ecr get-login --no-include-email --region $AWS_REGION)
+docker build -t $IMAGE .
+docker push $IMAGE
