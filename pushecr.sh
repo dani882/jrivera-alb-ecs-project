061dc2b345dc5ddecb1c8a376db2412bd89ddff1
Jesus Rivera <dani882@gmail.com>
2019-06-02 23:53:52 -0400

    fix master.yaml Output

--- pushecr.sh
+++ pushecr.sh
@@ -2,7 +2,7 @@
 
 # Script to login, create ecr repo and push docker image
 echo Login to ECR
-eval $(aws ecr get-login --region us-east-2 --no-include-email)
+eval $(aws ecr get-login --no-include-email)
 
 
 
